52703e18baea46fc73c7435cbb28cab9687b4b39
iyanuloluwa-Miracle <iyanudina@gmail.com>
2024-07-17 11:51:47 +0100

    :bugs: fixes:user  controller

--- src/modules/User/user.controller.ts
+++ src/modules/User/user.controller.ts
@@ -1,6 +1,7 @@
 import { FastifyReply, FastifyRequest } from "fastify";
-import { createUser } from "./user.service";
-import { createUserInput } from "./user.schema";
+import { verifyPassword } from "../utils/hash";
+import { createUser, findUserByEmail, findUsers } from "./user.service";
+import { createUserInput,findUserByEmail } from "./user.schema";
 
 export async function registerUserHandler(
   request: FastifyRequest<{
@@ -18,4 +19,40 @@ export async function registerUserHandler(
     console.log(e);
     return reply.code(500).send(e);
   }
+}
+
+
+export async function loginHandler(
+  request: FastifyRequest<{
+    Body: LoginInput;
+  }>,
+  reply: FastifyReply
+) {
+  const body = request.body;
+
+  // find a user by email
+  const user = await findUserByEmail(body.email);
+
+  if (!user) {
+    return reply.code(401).send({
+      message: "Invalid email or password",
+    });
+  }
+
+  // verify password
+  const correctPassword = verifyPassword({
+    candidatePassword: body.password,
+    salt: user.salt,
+    hash: user.password,
+  });
+
+  if (correctPassword) {
+    const { password, salt, ...rest } = user;
+    // generate access token
+    return { accessToken: request.jwt.sign(rest) };
+  }
+
+  return reply.code(401).send({
+    message: "Invalid email or password",
+  });
 }
--- src/modules/User/user.schema.ts
+++ src/modules/User/user.schema.ts
@@ -28,7 +28,29 @@ const createUserResponseSchema = z.object({
 });
 
 
+
+const loginSchema = z.object({
+  email: z
+    .string({
+      required_error: "Email is required",
+      invalid_type_error: "Email must be a string",
+    })
+    .email(),
+  password: z.string(),
+});
+
+const loginResponseSchema = z.object({
+  accessToken: z.string(),
+});
+
+
+export type CreateUserInput = z.infer<typeof createUserSchema>;
+
+export type LoginInput = z.infer<typeof loginSchema>;
+
 export const { schemas: userSchemas, $ref } = buildJsonSchemas({
   createUserSchema,
   createUserResponseSchema,
+  loginSchema,
+  loginResponseSchema,
 });
